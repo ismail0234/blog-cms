0791aefd1e307dea7c54d9b67a076ab31209e504
İsmail <ismaiil_0234@hotmail.com>
2024-09-11 11:20:51 +0300

    Update 2014_10_11_210814_create_roles_table.php

--- database/migrations/2014_10_11_210814_create_roles_table.php
+++ database/migrations/2014_10_11_210814_create_roles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(128);
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
